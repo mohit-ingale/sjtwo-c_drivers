a58b11fe176c56356dc58ef2f77382b5a358771b
Mohit Ingale <mohitingale5@gmail.com>
2019-09-24 17:51:25 -0700

    Part2: Commit completely

--- lpc40xx_freertos_assignment/a_interrupts/a_interrupt.c
+++ lpc40xx_freertos_assignment/a_interrupts/a_interrupt.c
@@ -1,6 +1,6 @@
 #include "a_interrupt.h"
 
-static function__void_f a_gpio_callback[32];
+static function__void_f a_gpio_callback[2][32];
 
 static void a_enable_interrupt(A_PERIPHERAL_INTERRUPT *interrupt_config){
     lpc_peripheral__enable_interrupt(interrupt_config->peripheral,interrupt_config->peripheral_isr_callback);
@@ -29,19 +29,19 @@ static uint32_t findPosition(uint32_t n)
 static void a_interrupt_dispatcher(void){
     uint32_t interrupt_pin=0;
     if(interrupt_pin = findPosition((LPC_GPIOINT->IO0IntStatF) & 0xffffffff)){
-        a_gpio_callback[interrupt_pin-1]();
+        a_gpio_callback[0][interrupt_pin-1]();
         LPC_GPIOINT->IO0IntClr |= (1<<(interrupt_pin-1));
     }
     else if(interrupt_pin = findPosition((LPC_GPIOINT->IO0IntStatR) & 0xffffffff)){
-        a_gpio_callback[interrupt_pin-1]();
+        a_gpio_callback[0][interrupt_pin-1]();
         LPC_GPIOINT->IO0IntClr |= (1<<(interrupt_pin-1));
     }
     else if(interrupt_pin = findPosition((LPC_GPIOINT->IO2IntStatR) & 0xffffffff)){
-        a_gpio_callback[interrupt_pin-1]();
+        a_gpio_callback[1][interrupt_pin-1]();
         LPC_GPIOINT->IO2IntClr |= (1<<(interrupt_pin-1));
     }
     else if(interrupt_pin = findPosition((LPC_GPIOINT->IO2IntStatF) & 0xffffffff)){
-        a_gpio_callback[interrupt_pin-1]();
+        a_gpio_callback[1][interrupt_pin-1]();
         LPC_GPIOINT->IO2IntClr |= (1<<(interrupt_pin-1));
     }
 }
@@ -69,7 +69,13 @@ static void a_interrupt_init(A_PERIPHERAL_INTERRUPT *interrupt_config){
 
 void a_interrupt_create(A_PERIPHERAL_INTERRUPT *interrupt_type, lpc_peripheral_e periferal, 
                                 function__void_f peripheral_isr_callback, IO_PORT_PIN *interrupt_pin,int edge){
-    a_gpio_callback[interrupt_pin->pin_num] = peripheral_isr_callback;
+    if(interrupt_pin->port == LPC_GPIO0){
+        a_gpio_callback[0][interrupt_pin->pin_num] = peripheral_isr_callback;
+    }else if(interrupt_pin->port == LPC_GPIO0)
+    {
+        a_gpio_callback[1][interrupt_pin->pin_num] = peripheral_isr_callback;
+    }
+    
     interrupt_type->peripheral = periferal;
     interrupt_type->peripheral_isr_callback = a_interrupt_dispatcher;
     interrupt_type->interrupt_pin = interrupt_pin;
