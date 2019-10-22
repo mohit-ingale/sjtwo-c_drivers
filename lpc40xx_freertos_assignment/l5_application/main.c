eeb6e5ae04a61bd0f7a8bb4b9a1787e3fee3363a
Mohit Ingale <mohitingale5@gmail.com>
2019-10-22 00:39:17 -0700

    Producer Consumer and CLI Handler for TASK Control

--- lpc40xx_freertos_assignment/l5_application/main.c
+++ lpc40xx_freertos_assignment/l5_application/main.c
@@ -34,23 +34,21 @@ int main(void) {
 /*
 1. when both task are of equal priority
   a. producer sends the switch value and receiver receivers the value and prints it.
-  b. when producer is not sending the value, the receiver is waiting to to receving anything from queue
+  b. when producer is not sending the value, the receiver is waiting to recevie anything from queue
   c. When producer sends the data to queue, when the reciver task is scheduled to run, it will print the value
       received from the queue
+  d. We can see in output that the receiver is waiting for data in queue, Producer sends the data in the queue then receiver
+      reads the data from the queue
 2. When Producer is of higher priority and consumer is of lower priority
-  a. producer sends the switch value and receiver receivers is waiting for the data to arrive in queue
+  a. producer sends the switch value and receiver is waiting for the data to arrive in queue
     producer sleeps after sending the value, then consumer task reads the data from the queue and prints it.
   b. when producer is not sending the value, the receiver is not receving anything from queue it is waiting for data
     to arrive in the queue
-      
-
-
-
-
-
+    The output is similar to equal priority as consumer is of low priority it will wait for data and read data from queue
+    when high priority producer has completed everything
 3. When Consumer is of Higher priority than producer
     a. Consumer will be waiting on the queue and will sleep for 10 ticks
-    b. When producer will come into ready state and consumer is sleeping the producer will read data and put in queue
+    b. When producer will come into ready state and consumer is sleeping the producer will read switch and put in queue
     c. Immediately context switch will happen and then consumer task will wake up and read data from queue and print the
       output
     d. Then again consumer will sleep waiting for data on queue and producer will complete its remaining task of
@@ -60,20 +58,23 @@ int main(void) {
 
 Additional Question
 when wait ticks is equal to 0 in receive queue
+when consumer is of high priority
   a. Consumer will keep on waiting on the queue doing busy looping and consume cpu indefinitetly
   b. Producer will never get cpu to execute the code and send data to the queue, hence receiver would never get data
 
 Purpose of block time
-  a.
+  a. If data is not available in queue the task would sleep and wait till either timeout occurs or data is available
+     in the queue
+
 
 */
 static void a_producer(void *params) {
   switch_e data;
   while (1) {
     data = (switch_e)my_gpio_get(&a_switch_for_queue);
-    // printf("Data Sent Before = %i\n", data);
+    printf("Data Sent Before = %i\n", data);
     xQueueSend(xQueueProducer, (void *)&data, 0);
-    // printf("Data Sent After = %i\n", data);
+    printf("Data Sent After = %i\n", data);
     vTaskDelay(1000);
   }
 }
@@ -83,9 +84,9 @@ static void a_consumer(void *params) {
   while (1) {
     // while (xQueueReceive(xQueueProducer, &data, 10) != pdTRUE)
     //   ;
-    // printf("Data receive before = %i\n", data);
+    printf("Data receive before = %i\n", data);
     xQueueReceive(xQueueProducer, &data, portMAX_DELAY);
-    // printf("Data receive after = %i\n", data);
+    printf("Data receive after = %i\n", data);
     // vTaskDelay(1000);
   }
 }
